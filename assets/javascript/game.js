534e0a364216fe53f33ad69c2acc7922c4364ef1
Scout <therearespidersinthisemail@gmail.com>
2019-05-09 00:53:53 -0600

    added win and loss conditions with reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
 
 
-var targetScore = Math.floor((Math.random() * 120) + 19);
 var counter = 0;
 var wins = 0;
 var losses = 0;
@@ -15,56 +14,61 @@ $("#wins").text(wins);
 $("#losses").text(losses);
 
 
+var start = function() {
 
-var startGame = function() {
     for (var i =0; i < 4; i++) {
 
     var crystalNum = Math.floor((Math.random() * 12) + 1);
     var imgCrystal = $("<img>");
     imgCrystal.addClass("crystal-image");
-    imgCrystal.attr("src", "assets/images/1-rupee1.png");
+    imgCrystal.attr("src", "assets/images/greenRupee.png");
     imgCrystal.attr("data-crystalValue", crystalNum);
     $("#crystal").append(imgCrystal);
+    
+    }
+
+    var targetScore = Math.floor((Math.random() * 120) + 19);
+    $("#targetScore").text(targetScore);
 
 
-
- 
-$(".crystal-image").on("click", function() {
+    $(".crystal-image").on("click", function() {
 
     var crystalValue = ($(this).attr("data-crystalValue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
+    console.log(crystalValue);
     $("#playerScore").text(counter);
     
 
     if (counter === targetScore) {
         wins++;
         $("#wins").text(wins);
-        // reset();
         alert("Game Over");
+        reset();
 
     }
 
     else if (counter > targetScore) {
         losses++;
         $("#losses").text(losses);
-        // reset();
         alert("Game Over");
+        reset();
+
     } 
     
 
 });
 
-}};
+};
 
-//  var reset = function() {
-//     $("#playerScore").text(counter);
-//     $("#targetScore").text(targetScore);
-//     startGame();
+ var reset = function() {
+     $("#targetScore, #playerScore, #crystal").empty();
+     counter = 0;
+    start();
 
-//  };
+ };
 
- startGame ();
+ start();
 
 
 })
